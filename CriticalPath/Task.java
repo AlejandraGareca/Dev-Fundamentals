2a081162029824c8a61e442ea24c62a009e1d88e
DEV OPS Capacitacion <DEVOPS@fundacion-jala.local>
2017-05-18 15:06:25 -0400

    Class commit

---
+++ CriticalPath/Task.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Task here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Task
+{
+    private String description;
+    public Task(){
+        description = "";
+    }
+    public void setDescription(String description){
+        this.description = description;
+        
+    }
+    public String getDescription(){
+        return description;
+    }
+}
---
+++ CriticalPath/TaskTest.java
@@ -0,0 +1,33 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class TaskTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class TaskTest
+{
+   @Test
+   public void createdInstanceOfTask(){
+       Task emptyTask = new Task ();
+       assertTrue(emptyTask instanceof Task);
+       assertTrue(emptyTask instanceof Object);
+   }
+   @Test
+   public void readDescriptionProperty(){
+       Task emptyTask = new Task();
+       assertEquals("", emptyTask.getDescription());
+   }
+   @Test
+   public void writeDescriptionProperty(){
+       Task task = new Task();
+       task.setDescription("This is a task");
+       assertEquals("This is a task", task.getDescription());
+   }
+}
